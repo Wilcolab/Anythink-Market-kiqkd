9708c4a002b263aea2629e229cc193c2b56cf7fb
michealgarcia <garciamicheal0@outlook.com>
2022-08-30 23:01:02 -0700

    double-checked code, fixed issue with missing hotfix

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -39,7 +39,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image}
+        src={item.image ? this.item : placeholder}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
