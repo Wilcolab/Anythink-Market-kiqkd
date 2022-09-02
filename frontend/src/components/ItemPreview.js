f7c83b584fb915647b96c73afb491cae799ae96d
michealgarcia <garciamicheal0@outlook.com>
2022-09-01 17:30:36 -0700

    ran prettier on frontend sub-directory

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -5,8 +5,6 @@ import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
 import placeholder from "../imgs/placeholder.png";
 
-
-
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
     dispatch({
